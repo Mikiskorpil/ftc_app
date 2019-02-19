0ceee9732c043b671907528eb127efb815b728b1
MartinMatura <martin.matura123@gmail.com>
2019-02-19 17:08:03 +0100

    Hello there, pokusili jsme se o začátek s auto modem, vše by mělo být popsané přímo v kódu, kdyby něco tak napište

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Automatic_Encoder_Op_Mode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Automatic_Encoder_Op_Mode.java
@@ -6,11 +6,14 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-    @Autonomous(name="Pushbot: Auto Drive By Encoder", group="Pushbot")
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
+@Autonomous(name="Pushbot: Auto Drive By Encoder", group="Pushbot")
 //@Disabled
     public class Automatic_Encoder_Op_Mode extends LinearOpMode {
 
         /* Declare OpMode members. */
+        HardwarePushbot         robot   = new HardwarePushbot();   // Use a Pushbot's hardware
         private ElapsedTime runtime = new ElapsedTime();
         private DcMotor rightDrive = null;
         private DcMotor leftDrive = null;
@@ -62,6 +65,22 @@ import com.qualcomm.robotcore.util.ElapsedTime;
             leftDrive.setTargetPosition(2240);
             rightDrive.setTargetPosition(2240);
 
+            //ZAČÁTEK NA STRANĚ S KRÁTEREM
+            //Základní trasa, otočky můžeme dopsat až s definitivně připevněnýma kolečkama
+            //Vzdálenosti jsou jenom odhady by oko podle obrázků
+
+            encoderDrive(DRIVE_SPEED, 24, 24,0);
+            //před 3 minerálama, Truny přinesl jeden Colour Sensor, můžeme zkusit příště
+            encoderDrive(TURN_SPEED, );
+            encoderDrive(DRIVE_SPEED, 36, 36,0);
+            //zatáčka na cestě do team čtverce v rohu
+            encoderDrive(TURN_SPEED, );
+            encoderDrive(DRIVE_SPEED, 52, 52, 0);
+            //team čtverec v rohu, musíme vyhodit marker
+            encoderDrive(TURN_SPEED,);
+            encoderDrive(DRIVE_SPEED,80, 80, 0);
+            //cesta do kráteru
+
             while (leftDrive.isBusy() && opModeIsActive()) {
 
             }
@@ -77,5 +96,48 @@ import com.qualcomm.robotcore.util.ElapsedTime;
             telemetry.addData("Path", "Complete");
             telemetry.update();
         }
+        public void encoderDrive(double speed,
+                                double leftInches, double rightInches,
+                                double timeoutS) {
+            int newLeftTarget;
+            int newRightTarget;
+
+            // Ensure that the opmode is still active
+            if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = robot.leftDrive.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRightTarget = robot.rightDrive.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            robot.leftDrive.setTargetPosition(newLeftTarget);
+            robot.rightDrive.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.leftDrive.setPower(Math.abs(speed));
+            robot.rightDrive.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (robot.leftDrive.isBusy() && robot.rightDrive.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1", "Running to %7d :%7d", newLeftTarget, newRightTarget);
+                telemetry.addData("Path2", "Running at %7d :%7d",
+                        robot.leftDrive.getCurrentPosition(),
+                        robot.rightDrive.getCurrentPosition());
+                telemetry.update();
+            }
+        }
     }
+}
 
